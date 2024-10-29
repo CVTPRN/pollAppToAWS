74512394066ee11a8eba5f73c7c70d62170b7be7
Mészáros Regő <100156040+CVTPRN@users.noreply.github.com>
2024-10-29 08:05:33 +0100

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,6 +2,14 @@ variable "aws_region" {
   default = "eu-central-1"
 }
 
+variable "access_key" {
+  default = "AKIARHQBNDNVZD4DPXEU"
+}
+
+variable "secret_key" {
+  default = "9W6msLr7EnHieLDuxAFj6gqzAmQXHJGADqwQ7e/e"
+}
+
 variable "db_username" {
   default = "admin"
 }
@@ -16,4 +24,12 @@ variable "public_key_path" {
 
 variable "private_key_path" {
   default = "~/.ssh/awspollapp_rsa"
+}
+
+variable "s3_bucket_name" {
+  default = "s3bucketpollapp"
+}
+
+variable "sender_email" {
+  default = "meszaros@regomeszaros.com"
 }
